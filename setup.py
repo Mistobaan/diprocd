a24d00ae824a178a43d59a3b6166308c64f1b95d
Loïc d'Anterroches <titoo@users.sourceforge.net>
2012-02-17 13:42:55 +0100

    Added the init scripts.

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ setup(name='diprocd',
       description='Distributed Process Control Daemon',
       author='Loic d\'Anterroches',
       author_email='open@danterroches.org',
-      url='http://www.ceondo.com/',
+      url='http://projects.ceondo.com/p/diprocd/',
       packages=['diprocd', 'diprocd.utils'],
       package_dir = {'diprocd': 'lib'},
-      scripts=['tools/dpd-clientd', 'tools/dpd-masterd', 'tools/dpd-workerd']
+      scripts=['tools/dpd-clientd', 'tools/dpd-masterd', 'tools/dpd-workerd'],
+      data_files=[('/etc/default', ['init.d/diprocd']),
+                  ('/etc/init.d', ['init.d/dpd-clientd', 'init.d/dpd-workerd', 'init.d/dpd-masterd']),
+                  ]
      )
